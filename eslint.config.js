28c056f5d4cfb49596e7240f009aa4be3ca9532a
USMAN GHANI <070usmanghani@gmail.com>
2026-09-16 12:19:40 +0500

    Fix tests and ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,25 @@
 const js = require("@eslint/js");
 
 module.exports = [
-    js.configs.recommended
+    js.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                require: "readonly",
+                module: "writable",
+                exports: "writable",
+                process: "readonly",
+                console: "readonly",
+                __dirname: "readonly",
+                __filename: "readonly",
+                describe: "readonly",
+                test: "readonly",
+                expect: "readonly",
+                beforeEach: "readonly",
+                afterEach: "readonly",
+                beforeAll: "readonly",
+                afterAll: "readonly"
+            }
+        }
+    }
 ];
